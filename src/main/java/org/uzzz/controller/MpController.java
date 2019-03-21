34c967087960aac8f2aa5dbceea3e1ea37e13ed5
fengjianglin <fjl@lenovo>
2019-03-21 20:03:38 +0800

    mp

---
+++ src/main/java/org/uzzz/controller/MpController.java
@@ -0,0 +1,34 @@
+package org.uzzz.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.uzzz.bean.MpSource;
+import org.uzzz.dao.MpSourceDao;
+
+@Controller
+@RequestMapping("mp")
+public class MpController {
+
+	@Autowired
+	private MpSourceDao mpSourceDao;
+
+	@PostMapping
+	@ResponseBody
+	public String receive(String host, String path, String contentType, String source) {
+		if (StringUtils.isNotBlank(host) //
+				&& host.startsWith("https://mp.weixin.qq.com/s?")) {
+			MpSource mpSource = new MpSource();
+			mpSource.setContentType(contentType);
+			mpSource.setHost(host);
+			mpSource.setPath(path);
+			mpSource.setSource(source);
+			mpSourceDao.save(mpSource);
+		}
+		return "OK";
+	}
+
+}
--- src/main/java/org/uzzz/dao/PostDao.java
+++ src/main/java/org/uzzz/dao/PostDao.java
@@ -13,8 +13,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.uzzz.bean.Post;
 
+@Repository
 public interface PostDao extends JpaRepository<Post, Long> {
 	@Query("select id from Post")
 	List<Long> findAllIds();
