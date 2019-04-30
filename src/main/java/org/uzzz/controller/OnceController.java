ee04435d55d49550a43bfddba15534864704fa8b
fengjianglin <fjl@lenovo>
2019-04-30 10:02:46 +0800

    ava opt

--- src/main/java/org/uzzz/controller/OnceController.java
+++ src/main/java/org/uzzz/controller/OnceController.java
@@ -1,10 +1,37 @@
 package org.uzzz.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.uzzz.bean.User;
+import org.uzzz.crawlers.CsdnCrawler;
+import org.uzzz.dao.UserDao;
 
 @Controller
 @RequestMapping("once")
 public class OnceController {
+
+	@Autowired
+	private UserDao userDao;
+
+	@GetMapping("ava")
+	@ResponseBody
+	public String ava() throws IOException {
+		List<User> users = userDao.findAll();
+		for (User user : users) {
+			String ava = user.getAvatar();
+			if (ava.startsWith("https://uzstatic.belost.xyz")) {
+				ava = ava.substring("https://uzstatic.belost.xyz".length());
+				user.setAvatar(ava);
+				userDao.save(user);
+			}
+		}
+		return "OK";
+	}
 
 }
